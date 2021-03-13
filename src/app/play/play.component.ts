b5d21619cfc9c0aec6847b3018d4d68e3217cf47
Ryan Bressette <bressetteryan@gmail.com>
2021-03-12 19:11:50 -0500

    update

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,10 +1,10 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Platform } from '@ionic/angular'
 import { EntityDrawer } from 'src/domain/app-domain/entity-drawer';
-import { InputHandler } from 'src/domain/app-domain/input-handler';
+import { PlayerMover } from 'src/domain/app-domain/player-mover';
 import { Entity, Location } from 'src/domain/server-domain/entity';
 import { Ship } from 'src/domain/server-domain/ship';
-import { Server } from '../../domain/server-domain/server';
+import { GameServer } from '../../domain/server-domain/game-server';
 
 @Component({
   selector: 'app-play',
@@ -14,26 +14,42 @@ import { Server } from '../../domain/server-domain/server';
 export class PlayComponent implements AfterViewInit {
   @ViewChild('imageCanvas', { static: false }) canvas: any;
 
+  @HostListener('document:keyup', ['$event'])
+  handleKeyUpEvent(event: KeyboardEvent) { 
+    this.playerMover.keyUp(event);
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyDownEvent(event: KeyboardEvent) {
+    this.playerMover.keyDown(event);
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickEvent(event: MouseEvent) {
+    this.playerMover.click(event);
+  }
+
   canvasElement: any;
   saveX: number;
   saveY: number;
   scale: number = 0.5;
   username: string = 'Ryan';
 
-  server: Server;
+  server: GameServer;
+  playerMover: PlayerMover;
   entityDrawer: EntityDrawer;
-  inputHandler: InputHandler;
 
   constructor(private plt: Platform) {
-    
-    this.server = new Server();
+
+    this.server = new GameServer();
     this.server.addMe(this.username);
     this.server.joinGame(this.username);
 
+    this.playerMover = new PlayerMover(this.server, this.username);
+
     this.entityDrawer = new EntityDrawer();
-    this.inputHandler = new InputHandler(window);
   }
- 
+
   ngAfterViewInit() {
 
     // TODO: Register window event listeners
@@ -45,19 +61,19 @@ export class PlayComponent implements AfterViewInit {
 
     this.run();
   }
- 
+
   canvasClicked(ev) {
     console.log('Start drawing');
     let canvasPosition = this.canvasElement.getBoundingClientRect();
-    
+
     console.log(`ev: (${ev.pageX}, ${ev.pageY})`);
     console.log(canvasPosition);
- 
+
     let clickX = ev.pageX * 2 - canvasPosition.x * 2;
     let clickY = ev.pageY * 2 - canvasPosition.y;
 
     let ctx = this.canvasElement.getContext('2d');
-    
+
     ctx.beginPath();
     ctx.arc(clickX, clickY, 10, 0, 2 * Math.PI);
     ctx.stroke();
@@ -69,39 +85,39 @@ export class PlayComponent implements AfterViewInit {
    */
   run() {
     this.server.updateEntities();
-    let data =  this.server.getMyShipAndSurroundingObjects(this.username);
+    let data = this.server.getMyShipAndSurroundingObjects(this.username);
     this.draw(data);
   }
 
   turnLeft() {
     console.log('app.turnLeft()');
     this.server.turnLeft(this.username);
-    let data =  this.server.getMyShipAndSurroundingObjects(this.username);
+    let data = this.server.getMyShipAndSurroundingObjects(this.username);
     this.draw(data);
   }
 
   turnRight() {
     console.log('app.turnRight()');
     this.server.turnRight(this.username);
-    let data =  this.server.getMyShipAndSurroundingObjects(this.username);
+    let data = this.server.getMyShipAndSurroundingObjects(this.username);
     this.draw(data);
   }
 
   forward() {
     console.log('app.forward()');
     this.server.forward(this.username);
-    let data =  this.server.getMyShipAndSurroundingObjects(this.username);
+    let data = this.server.getMyShipAndSurroundingObjects(this.username);
     this.draw(data);
   }
 
   backward() {
     console.log('app.backward()');
     this.server.backward(this.username);
-    let data =  this.server.getMyShipAndSurroundingObjects(this.username);
+    let data = this.server.getMyShipAndSurroundingObjects(this.username);
     this.draw(data);
   }
 
-  draw(data: {ship: Ship, objects: Entity[]}) {
+  draw(data: { ship: Ship, objects: Entity[] }) {
 
     let ctx: CanvasRenderingContext2D = this.canvasElement.getContext('2d');
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -109,33 +125,17 @@ export class PlayComponent implements AfterViewInit {
     let xMiddle = ctx.canvas.width / 2;
     let yMiddle = ctx.canvas.height / 2;
 
-    ctx.beginPath();
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "red";
-    let shipAngle = data.ship.velocity.h * Math.PI / 180;
-    
-    // Transform context
-    ctx.translate(xMiddle, yMiddle);
-    ctx.rotate(shipAngle);
-
-    this.entityDrawer.drawPlayerShip(ctx, data.ship);
-
-    // Draw visibility circle
-    ctx.arc(0, 0, data.ship.visibility, 0, 2 * Math.PI);
-    ctx.stroke();
-
-    // Reset rotation
-    ctx.rotate(-shipAngle);
+    this.entityDrawer.drawPlayerShip(ctx, data.ship, xMiddle, yMiddle);
 
     data.objects.forEach(entity => {
-      let renderLocation = new Location(entity.location.x - data.ship.location.x, entity.location.y - 
-                                        data.ship.location.y);
+      let renderLocation = new Location(entity.location.x - data.ship.location.x, entity.location.y -
+        data.ship.location.y);
       this.entityDrawer.drawEntity(ctx, entity, renderLocation);
     })
 
     ctx.translate(-xMiddle, -yMiddle);
-    
+
   }
-  
+
 }
 
--- src/domain/app-domain/entity-drawer.ts
+++ src/domain/app-domain/entity-drawer.ts
@@ -27,6 +27,7 @@ export class EntityDrawer {
    * @param entity 
    */
   drawEntity(ctx: CanvasRenderingContext2D, entity: Entity, renderLocation: Location): void {
+    ctx.strokeStyle = "red";
     if (entity instanceof Ship) {
       this.drawShip(ctx, entity, renderLocation);
     } else if (entity instanceof Rock) {
@@ -39,9 +40,26 @@ export class EntityDrawer {
    * @param ctx The canvas context to draw to
    * @param ship The player's ship to draw
    */
-  drawPlayerShip(ctx: CanvasRenderingContext2D, ship: Ship): void {
+  drawPlayerShip(ctx: CanvasRenderingContext2D, ship: Ship, x: number, y: number): void {
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    let shipAngle = ship.velocity.h * Math.PI / 180;
+    
+    // Transform context
+    ctx.translate(x, y);
+    ctx.rotate(shipAngle);
+    ctx.fillStyle = "blue";
     ctx.fillRect(-ship.width / 2, -ship.height / 2, ship.width, ship.height);
     ctx.stroke();
+
+    // Draw visibility circle
+    ctx.arc(0, 0, ship.visibility, 0, 2 * Math.PI);
+    ctx.strokeStyle = "blue";
+    ctx.stroke();
+
+    // Reset rotation
+    ctx.rotate(-shipAngle);
   }
 
 }
